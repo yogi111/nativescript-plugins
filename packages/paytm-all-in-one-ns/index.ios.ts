88de10b6e35fe3a921396b60f94b1a194a1e5a3f
Govinda Mac <govi2010@gmail.com>
2023-10-21 17:12:59 +0530

    commit IOS fixes

--- packages/paytm-all-in-one-ns/index.ios.ts
+++ packages/paytm-all-in-one-ns/index.ios.ts
@@ -14,14 +14,17 @@ export class PaytmAllInOneNsDelegateImp extends NSObject implements AIDelegate {
   didFinishWithResponse(status: AIPaymentStatus, response: NSDictionary<string, any>): void {
     const responseJson = {};
     const data: string[] = Array.from(response.allKeys);
-    console.log(data);
-    data.forEach((key) => {
-      responseJson[key] = response.valueForKey(key);
-    });
-    PaytmAllInOneNs.resolve(responseJson as PaytmResponse);
+    if (data.length === 1 && data[0] === 'response') {
+      PaytmAllInOneNs.reject(new Error(response.valueForKey('response')));
+    } else {
+      data.forEach((key) => {
+        responseJson[key] = response.valueForKey(key);
+      });
+      PaytmAllInOneNs.resolve(responseJson as PaytmResponse);
+    }
   }
   openPaymentWebVC(controller: UIViewController): void {
-    controller.modalPresentationStyle = UIModalPresentationStyle.FullScreen;
+    controller.modalPresentationStyle = UIModalPresentationStyle.Automatic;
     Application.ios.rootController.presentViewControllerAnimatedCompletion(controller, true, null);
   }
 }
@@ -40,6 +43,7 @@ export class PaytmAllInOneNs extends PaytmAllInOneNsCommon {
       //@ts-ignore
       this.nativeView = AIHandler.alloc().init();
       const delegate = PaytmAllInOneNsDelegateImp.initWithOwner(new WeakRef(this));
+      this.nativeView.restrictAppInvokeFlowWithRestrict(!option.enableAppInvoke);
       this.nativeView.openPaytmWithMerchantIdOrderIdTxnTokenAmountCallbackUrlDelegateEnvironmentMobileHashIsMobileVerificationRequiredUrlScheme(option.mid, option.orderid, option.txnToken, option.amount, option.callbackurl, delegate, option.isStagging ? AIEnvironment.Staging : AIEnvironment.Production, '', 'false', option.iosScheme);
     });
   }
